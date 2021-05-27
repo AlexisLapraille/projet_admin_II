ddcb3a1e1ebaab223aadd5b6c1c6d1332400d9c5
bauchau <HE201881@students.ephec.be>
2021-05-27 23:50:40 +0200

    redirect https

--- web-test/apache2/index.php
+++ web-test/apache2/index.php
@@ -1,4 +1,12 @@
-<!DOCTYPE html>
+<?php
+$mysql_config = parse_ini_file('mysql.ini');
+
+$hostname=$mysql_config['host'];;
+$user = $mysql_config['username'];;
+$password = $mysql_config['password'];;
+$database = $mysql_config['database'];;
+$table = "todo_list";
+?>
 <html>
 <head>
 	<title>B2B</title>
